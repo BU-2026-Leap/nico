532c386098abf494c48a68961ffa00030b7d787b
Nico B <nbonnetm@gmail.com>
2026-01-05 16:30:05 -0500

    Abstracted functions, created tests for functions

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ import os
 import json
 from pathlib import Path
 
-full_base_path = Path(__file__).resolve().parent
-input_filename = full_base_path / "test_scores.csv"
 
-output_filename = "output.json"
+def main():
+    full_base_path = Path(__file__).resolve().parent
+    input_filename = full_base_path / "test_scores.csv"
 
-if os.path.exists(output_filename):
-    os.remove(output_filename)
+    scores_list = read_csv(input_filename)
+    average_final, unique_students = generate_scores(scores_list)
 
-average_final = 0.0
-unique_students = 0
+    write_output(average_final, unique_students)
 
-with open(input_filename) as f:
-    reader = csv.DictReader(f)
+
+def read_csv(input_filename):
+    with open(input_filename) as f:
+        scores_list = []
+        reader = csv.DictReader(f)
+        for row in reader:
+            scores_list.append(row)
+        return scores_list
+
+
+def generate_scores(list):
     final_score = 0
     final_num = 0
     student_ids = []
-    for row in reader:
+    for row in list:
         print(row)
         # Add all final scores
         if row['exam_name'] == 'final':
@@ -27,26 +35,44 @@ with open(input_filename) as f:
             final_num += 1
         else:
             continue
-
         # Check for unique student ID
         if row['student_id'] not in student_ids:
             student_ids.append(row['student_id'])
+
     # Find average
-    average_final_float = final_score / final_num
     unique_students = len(student_ids)
-    average_final = f"{average_final_float:.2f}"
+    average_final = format_float(average(final_score, unique_students))
 
-    # Print
-    print(f"Number of students: {unique_students}")
-    print(f"Average final: {average_final}")
+    return average_final, unique_students
 
-if os.path.exists(output_filename):
-    os.remove(output_filename)
 
-result = {
-    "average_final": average_final,
-    "unique_students": unique_students,
-}
+def average(numerator, denominator):
+    try:
+        average = numerator/denominator
+    except ZeroDivisionError:
+        average = 0
+    return average
 
-with open(output_filename, "w") as out:
-    json.dump(result, out, indent=2)
+
+
+def format_float(float):
+    formatted_str = f"{float:.2f}"
+    return formatted_str
+
+
+def write_output(average_final, unique_students):
+    output_filename = "output.json"
+
+    if os.path.exists(output_filename):
+        os.remove(output_filename)
+
+    result = {
+        "average_final": average_final,
+        "unique_students": unique_students,
+    }
+
+    with open(output_filename, "w") as out:
+        json.dump(result, out, indent=2)
+
+if __name__ == "__main__":
+    main()
--- test_main.py
+++ test_main.py
@@ -3,6 +3,29 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+from main import average, format_float, generate_scores, write_output
+
+def test_average():
+    nominators = [0, 1, 5, 100, 1000]
+    denominators = [0, 1, 5, 100, 1000]
+    for nom in nominators:
+        for denom in denominators:
+            result = average(nom, denom)
+            if denom == 0 or nom == 0:
+                assert result == 0
+            elif denom == 1:
+                assert result == nom
+            elif denom == nom:
+                assert result == 1
+
+def test_format_float():
+    result_up = format_float(2.99999)
+    assert result_up == '3.00'
+
+    result_down = format_float(2.111111)
+    assert result_down == '2.11'
+
+
 
 def test_script_output():
     repo_root = Path(__file__).resolve().parents[0]
@@ -38,3 +61,4 @@ def test_script_output():
         # Check for correct number of students
         assert "unique_students" in main_output
         assert main_output["unique_students"] == 3
+
