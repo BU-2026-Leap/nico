5175f9f3b1f1f1127950782cc3be0a677e05df1a
Nico B <nbonnetm@gmail.com>
2026-01-06 14:58:48 -0500

    Started implementing continuous deployment

--- aws/lambda_app.py
+++ aws/lambda_app.py
@@ -2,19 +2,49 @@ from aws.s3_data_fetcher import S3DataFetcher
 from common.exam_data_processor import ExamDataProcessor
 from common.contracts import read_and_compute
 
+import base64
+import os
+
 def build_html(stats):
+  img_path = os.path.join(os.environ['LAMBDA_TASK_ROOT'],"common/doggy.JPG")
+
+  with open(img_path, "rb") as f:
+    data= base64.b64encode(f.read()).decode("utf-8")
+
     return f"""<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
     <title>Summary of All</title>
+    <style>
+      h1 {{
+        font-family: georgia;
+      }}
+      table {{
+        border-collapse: collapse;
+        width: 80%
+      }}
+      td, th {{
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+      }}
+    </style>
   </head>
   <body>
+    <img src="data:image/jpeg;base64,{data}" alt="silly dog">
+    <h1>Hiiiiiiiiiiiiiiiiiiii</h1>
     <h1>Student Scores!</h1>
-    <ul>
-      <li><strong>Average score:</strong> {stats.average_final}</li>
-      <li><strong>Unique students:</strong> {stats.unique_students}</li>
-    </ul>
+    <table>
+      <tr>
+        <th><strong>Average score:</strong></th>
+        <th><strong>Unique students:</strong></th>
+      </tr>
+      <tr>
+        <td>{stats.average_final}</td>
+        <td>{stats.unique_students}</td>
+      </tr>
+    </table>
   </body>
 </html>
 """
