e5268895e54787011b5824916c8035deed5885a6
Nico B <nbonnetm@gmail.com>
2026-01-07 14:36:30 -0500

    Modified layouts

--- aws/layouts.py
+++ aws/layouts.py
@@ -8,6 +8,8 @@ def html_response(body, status=200):
         <!DOCTYPE html>
         <html>
         <head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+            <link href="aws/styles.css" rel="stylesheet">
             <style>
                 body {{ font-family: Arial; max-width: 600px; margin: 50px auto; }}
                 img {{ max-width: 100%; height: auto; }}
@@ -26,7 +28,20 @@ def page_template(title, content, image_url=None):
     """Reusable page template"""
     img_tag = f'<img src="{image_url}" alt="{title}">' if image_url else ''
     return f"""
-        <h2>{title}</h2>
-        {img_tag}
-        <div>{content}</div>
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <h2>{title}</h2>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="frame mb-3 mt-3">
+                    <div class="img-container col-auto border border-white">
+                      {img_tag}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flavortext">{content}</div>
     """
+
